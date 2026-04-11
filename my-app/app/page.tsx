729e1856c96da7e89c888f66d0a104f95aa6027a
yuchane0824-hue <yuchane0824@gmail.com>
2026-04-12 00:23:40 +0800

    feat: page update

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,9 +1,17 @@
-import Image from "next/image";
-import Link from "next/link";
+"use client"
+import Menu from "../component/menu"
 
 export default function Home() {
 
   return (
-    <></>
+    <div className="flex h-full justify-center">
+      <Menu />
+
+      <div className="w-full sm:block hidden" style={{backgroundColor: '#EFF0EA'}}>
+        <p className="flex flex-col justify-center items-center text-lg font-bold ">
+          梁育禎的個人履歷首頁
+        </p>
+      </div>
+    </div>
   );
 }
